66044cdd14902f7277faa75de46572cb6f1dc6dc
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-09-04 20:44:57 +0800

    add cert export from minikube

--- setup-all.bash
+++ setup-all.bash
@@ -9,6 +9,10 @@ KICS_DOCKER_IMAGE_IS_PULLED="$(docker images -q checkmarx/kics)"
 [[ -n "$KICS_DOCKER_IMAGE_IS_PULLED" ]] || docker pull checkmarx/kics:latest
 [[ -n "$MINIKUBE_IS_SET_UP" ]] || ( minikube start --profile dev && minikube profile dev && minikube addons enable ingress && skaffold config set --global local-cluster true )
 
+mkdir -p "$SCRIPT_DIR/certs"
+openssl s_client -showcerts -connect "$(minikube ip):443" </dev/null |  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "$SCRIPT_DIR/certs/ingress-cert.pem"
+openssl x509 -pubkey -noout -in "$SCRIPT_DIR/certs/ingress-cert.pem"  > "$SCRIPT_DIR/certs/ingress-pubkey.pem"
+
 cd "$SCRIPT_DIR/docker"
 docker-compose down
 docker-compose up --build reverseproxy --build mockpass -d
