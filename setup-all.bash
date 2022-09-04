b31dd21e4a41b90ab4de12d06fc29b27d053c0dc
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-09-05 00:48:59 +0800

    dynamically generate certs

--- setup-all.bash
+++ setup-all.bash
@@ -9,15 +9,20 @@ KICS_DOCKER_IMAGE_IS_PULLED="$(docker images -q checkmarx/kics)"
 [[ -n "$KICS_DOCKER_IMAGE_IS_PULLED" ]] || docker pull checkmarx/kics:latest
 [[ -n "$MINIKUBE_IS_SET_UP" ]] || ( minikube start --profile dev && minikube profile dev && minikube addons enable ingress && skaffold config set --global local-cluster true )
 
-KEYCLOAK_TRUSTSTORE_PATH="$SCRIPT_DIR/docker/keycloak/keycloak-truststore.p12"
+KEYCLOAK_STORE_PATH="$SCRIPT_DIR/docker/keycloak/stores"
 MINIKUBE_CERT_PATH="$SCRIPT_DIR/docker/mockpass/certs"
-NGINX_CERT_PATH="$SCRIPT_DIR/docker/reverseproxy"
+NGINX_CERT_PATH="$SCRIPT_DIR/docker/reverseproxy/certs"
 
 mkdir -p "$MINIKUBE_CERT_PATH"
 openssl s_client -showcerts -connect "$(minikube ip):443" </dev/null |  sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > "$MINIKUBE_CERT_PATH/ingress-cert.pem"
 openssl x509 -pubkey -noout -in "$MINIKUBE_CERT_PATH/ingress-cert.pem"  > "$MINIKUBE_CERT_PATH/ingress-pubkey.pem"
 
-keytool -importcert -storetype PKCS12 -keystore "$KEYCLOAK_TRUSTSTORE_PATH" -storepass changeit -alias minikube -file "$NGINX_CERT_PATH/nginx-selfsigned.crt" -noprompt
+mkdir -p "$NGINX_CERT_PATH"
+openssl req -x509 -nodes -days 365 -subj "/C=CA/ST=QC/O=Company, Inc./CN=*" -addext "subjectAltName=DNS:localhost,IP:192.168.49.1" -newkey rsa:2048 -keyout "$NGINX_CERT_PATH/nginx-selfsigned.key" -out "$NGINX_CERT_PATH/nginx-selfsigned.crt"
+
+mkdir -p $KEYCLOAK_STORE_PATH
+rm "$KEYCLOAK_STORE_PATH/keycloak-truststore.p12" || true
+keytool -importcert -storetype PKCS12 -keystore "$KEYCLOAK_STORE_PATH/keycloak-truststore.p12" -storepass changeit -alias minikube -file "$NGINX_CERT_PATH/nginx-selfsigned.crt" -noprompt
 
 cd "$SCRIPT_DIR/docker"
 docker-compose down
