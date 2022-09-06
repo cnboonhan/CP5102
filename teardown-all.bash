3d59c70de2f415a024dd7dc742c5d80e4df47aee
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-09-06 23:40:23 +0800

    add cleanup

--- teardown-all.bash
+++ teardown-all.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+set -o xtrace
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
@@ -10,3 +11,7 @@ cd "$SCRIPT_DIR/docker"
 docker-compose down
 
 docker system prune
+
+rm "$SCRIPT_DIR/keycloak/realms.json" || true
+rm -r "$SCRIPT_DIR/keycloak/stores" || true
+rm -r "$SCRIPT_DIR/reverseproxy/certs" || true
