5f866107fd573e5f4895dfdee462a0424f75c0d7
Charayaphan Nakorn Boon Han <charayaphan.nakorn.boon.han@gmail.com>
2022-10-07 00:34:47 +0800

    add gitea

--- post-deploy-exec.bash
+++ post-deploy-exec.bash
@@ -2,5 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-#kubectl exec services/gitea-svc -- su - git -c '/usr/local/bin/gitea admin user create --admin --username root --password password --email admin@example.com'
-#kubectl exec services/gitea-svc -- su - git -c "/usr/local/bin/gitea admin auth add-oauth --name gitea --provider openidConnect -key gitea --secret hJzobbxpUODhfBF6a3gHLk1TEp5o8cjK --auto-discover-url http://$(minikube ip)/auth/realms/hello-world/.well-known/openid-configuration"
+ROOT_IS_CREATED="$(kubectl exec services/gitea-svc -- su - git -c '/usr/local/bin/gitea admin user list')"
+[[ "$ROOT_IS_CREATED" =~ "root" ]] || kubectl exec services/gitea-svc -- su - git -c "/usr/local/bin/gitea admin user create --admin --username root --password password --email admin@example.com" 
+OAUTH_IS_CREATED="$(kubectl exec services/gitea-svc -- su - git -c '/usr/local/bin/gitea admin auth list')"
+[[ "$OAUTH_IS_CREATED" =~ "gitea" ]] || kubectl exec services/gitea-svc -- su - git -c "/usr/local/bin/gitea admin auth add-oauth --name gitea --provider openidConnect -key gitea --secret 00000000-0000-0000-0000-000000000000 --auto-discover-url http://cluster.cp5102.edu/auth/realms/hello-world/.well-known/openid-configuration"
